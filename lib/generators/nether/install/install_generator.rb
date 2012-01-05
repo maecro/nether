40adf3b6b89ef29a635da5679531b890e3d54adc
Matthew Cross <mattcross88@gmail.com>
2012-01-05 21:58:50 +1000

    Updated filename to include erb extension.

--- lib/generators/nether/install/install_generator.rb
+++ lib/generators/nether/install/install_generator.rb
@@ -2,7 +2,7 @@ module Nether
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
     def install_pageless
-      copy_file "jquery.pageless.js", "app/assets/javascripts/jquery.pageless.js"
+      copy_file "jquery.pageless.js.erb", "app/assets/javascripts/jquery.pageless.js.erb"
       copy_file "load.gif", "app/assets/images/load.gif"
     end
   end
