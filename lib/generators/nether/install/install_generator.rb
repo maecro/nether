6c1cc76591a18b25b8d04ce31fcb85e6f523ad34
Matthew Cross <mattcross88@gmail.com>
2012-01-05 21:43:06 +1000

    Fixed thor command.

--- lib/generators/nether/install/install_generator.rb
+++ lib/generators/nether/install/install_generator.rb
@@ -1,8 +1,8 @@
 module Nether
   class InstallGenerator < Rails::Generators::Base
     def install_pageless
-      copy "templates/jquery.pageless.min.js", "app/assets/javascripts/jquery.pageless.min.js"
-      copy "templates/load.gif", "app/assets/images/load.gif"
+      copy_file "templates/jquery.pageless.min.js", "app/assets/javascripts/jquery.pageless.min.js"
+      copy_file "templates/load.gif", "app/assets/images/load.gif"
     end
   end
 end
