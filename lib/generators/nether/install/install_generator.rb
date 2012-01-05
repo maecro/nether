5a30789c565cf9f608b0cb767b091e22cc42e8d7
Matthew Cross <mattcross88@gmail.com>
2012-01-05 21:51:55 +1000

    Added source root.

--- lib/generators/nether/install/install_generator.rb
+++ lib/generators/nether/install/install_generator.rb
@@ -1,5 +1,6 @@
 module Nether
   class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
     def install_pageless
       copy_file "templates/jquery.pageless.min.js", "app/assets/javascripts/jquery.pageless.min.js"
       copy_file "templates/load.gif", "app/assets/images/load.gif"
