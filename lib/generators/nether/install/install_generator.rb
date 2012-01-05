3f21258469dc2ef1aa050f896fe61513ef92ea6d
Matthew Cross <mattcross88@gmail.com>
2012-01-05 21:55:55 +1000

    Updated filename.

--- lib/generators/nether/install/install_generator.rb
+++ lib/generators/nether/install/install_generator.rb
@@ -2,7 +2,7 @@ module Nether
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
     def install_pageless
-      copy_file "jquery.pageless.min.js", "app/assets/javascripts/jquery.pageless.min.js"
+      copy_file "jquery.pageless.js", "app/assets/javascripts/jquery.pageless.js"
       copy_file "load.gif", "app/assets/images/load.gif"
     end
   end
